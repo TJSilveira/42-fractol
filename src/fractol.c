39a77b94e8e81cf6b95c8c55daddd3f73569297e
Tiago Silveira <tiagojesusilveira@gmail.com>
2025-05-30 11:29:08 +0200

    First Mandelbrot set print

--- src/fractol.c
+++ src/fractol.c
@@ -19,51 +19,57 @@ int	close_win(t_engine engine)
 	return (0);
 }
 
-int		in_scope(t_coor *c)
+void	next_coor(t_coor *total, t_coor c)
 {
-	if (pow(c->x,2) + pow(c->y, 2) > 2)
-	{
-		return (1);
-	}
-	return (0);
+	double	temp_x;
+	
+	temp_x = total->x * total->x - total->y * total->y + c.x;
+	total->y = 2. * total->x * total->y + c.y;
+	total->x = temp_x;
 }
 
-void	fractal_calc(t_coor *c)
+int	fractal_calc(t_coor c)
 {
-	int	i;
-	// double	total;
+	int		i;
+	t_coor	total;
 
-	i = 0;
-	while (i < 20)
+	i = 1;
+	total.x = 0;
+	total.y = 0;
+	while (i < 50)
 	{
-		/* code */
+		next_coor(&total, c);
+		if (pow(total.x,2) + pow(total.y, 2) > 20000)
+		{
+			break;
+		}
 		i++;
 	}
-	c->x = 0;
+	// printf("x:%f y:%f %i\n", total.x, total.y, i);
+	return (i);
 }
 
 void	update_pixel(t_image *img/*, t_frac f*/)
 {
 	t_pixel		p;
 	t_coor		c;
+	int			w;
 
 	p.x = 0;
-	while (p.x < img->line_len/4) 
+	w = img->line_len/4;
+	while (p.x < w) 
 	{
 		p.y = 0;
 		while (p.y < img->height)
 		{
-			p.clr = 0x00FFFF00;
+			c.y = img->top_left.y + (double)p.y / (double)img->height * (img->bot_right.y - img->top_left.y);
+			c.x = img->top_left.x + (double)p.x / (double)w * (img->bot_right.x - img->top_left.x);
+			// printf("x:%f y:%f\n", c.x, c.y);
+			if (fractal_calc(c) == 50)
+				p.clr = 0xFF00000;
+			else
+				p.clr = 0x00FFFF00;
 			my_mlx_pixel_put(img, p.x, p.y, p.clr);
-			c.y = p.y / img->height * (img->top_left.y - img->bot_right.y);
-			c.x = p.x / img->height * (img->top_left.x - img->bot_right.x);
-
-
-			/*if (pow(p.x-200,2) + pow(p.y-200, 2) <= 8000 && pow(p.x-200,2) + pow(p.y-200, 2) >= 3600)
-			{
-				p.clr = 0x0000FF00;
-				my_mlx_pixel_put(img, p.x, p.y, p.clr);
-			}*/
 			p.y += 1;
 		}
 		p.x += 1;
@@ -75,7 +81,10 @@ void	init_img(t_image *img, t_engine *engine)
 	img->height = 800;
 	img->img = mlx_new_image(engine->mlx, 1200, img->height);
 	img->addr = mlx_get_data_addr(img->img, &img->pix_bits, &img->line_len, &img->endian);
-
+	img->top_left.x = -3;
+	img->top_left.y = 1.5;
+	img->bot_right.x = 1;
+	img->bot_right.y = -1.5;
 }
 
 int	main(void)
